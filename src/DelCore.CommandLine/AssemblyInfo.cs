4ad3ce93938df3c03f6f2225f6922763e42d535e
Shane Delany <delscid@gmail.com>
2017-09-28 02:34:51 +1300

    Shared internals between libraries

---
+++ src/DelCore.CommandLine/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿#region Copyright 2017 Shane Delany
+
+// Filename:  AssemblyInfo.cs
+// Modified:  26/09/2017
+
+#endregion
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DelCore.CommandLine.Tests")]
