b2fbf63a0e63fcc40a266cf4829fad6ce6d405db
Shane Delany <delscid@gmail.com>
2017-09-30 01:28:15 +1300

    Added missing enumerable.

---
+++ src/DelCore.CommandLine/ArgumentType.cs
@@ -0,0 +1,18 @@
+﻿#region Copyright 2017 Shane Delany
+
+// Filename:  ArgumentType.cs
+// Modified:  30/09/2017
+
+#endregion
+
+namespace DelCore.CommandLine
+{
+    public enum ArgumentType
+    {
+        String,
+        Boolean,
+        Integer,
+        File,
+        Enumerable
+    }
+}
